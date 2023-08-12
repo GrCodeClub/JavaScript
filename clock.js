35923b8e4a1341bb29bb2416ebff7461680df236
Gr Code Club <135416047+GrCodeClub@users.noreply.github.com>
2023-08-12 14:44:40 +0300

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,3 +1,5 @@
+//  clock.js
+//===================
 //Αυτή η συνάρτηση θα αναλάβει να ενημερώσει την τρέχουσα ώρα στο στοιχείο HTML.
 function displayTime() {
     // αποθηκεύεται το στοιχείο HTML με το αναγνωριστικό "clock" στη μεταβλητή element.
@@ -8,18 +10,26 @@ function displayTime() {
     var hours = currentTime.getHours();
     var minutes = currentTime.getMinutes();
     var seconds = currentTime.getSeconds();
-    
     // Προσθέστε μηδενικά μπροστά αν οι ώρες, οι λεπτά ή τα δευτερόλεπτα είναι μονοψήφια
     hours = (hours < 10 ? "0" : "") + hours;
     minutes = (minutes < 10 ? "0" : "") + minutes;
     seconds = (seconds < 10 ? "0" : "") + seconds;
-    
     // δημιουργείται μια συμβολοσειρά που περιέχει την τρέχουσα ώρα στη μορφή "ώρες:λεπτά:δευτερόλεπτα".
     var currentTimeString = hours + ":" + minutes + ":" + seconds;
-    
     // Αντικαθιστούμε το περιεχόμενο του element με την τρέχουσα ώρα
     element.innerHTML = currentTimeString;
-  }
-  
-  // Καλούμε τη συνάρτηση displayTime κάθε δευτερόλεπτο
-  setInterval(displayTime, 1000);
+    }
+// Καλούμε τη συνάρτηση displayTime κάθε δευτερόλεπτο
+setInterval(displayTime, 1000);
+
+
+//  clock.html
+//===================
+//<html>
+//  <head>
+//  </head>
+//  <body>
+//      <script src="clock.js"></script>
+//      <h1 id="clock"></h1>
+//  </body>
+//</html>
