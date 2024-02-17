389dfdbe65a6c4728dcdfe42592e1ed87e783e1c
grcodeclub <135416047+GrCodeClub@users.noreply.github.com>
2024-02-17 21:59:47 +0200

    Update search_table_row_cells_0.js

--- search/search_table_row_cells_0.js
+++ search/search_table_row_cells_0.js
@@ -4,14 +4,18 @@ const rows = table.getElementsByTagName("tr");
 
 searchInput.addEventListener("input", function() {
   const searchValue = removeAccents(searchInput.value.toLowerCase());
+  console.log("Search Value:", searchValue);
   const searchPhrases = searchValue.match(/\b[\w'-]+\b/g); // Φράσεις με βάση τα κενά
+  console.log("Search Phrases:", searchPhrases);
 
   Array.from(rows).forEach(function(row, index) {
     let match = false;
     const firstCellText = removeAccents(row.cells[0].textContent.toLowerCase());
+    console.log("Cell Text:", firstCellText);
 
     searchPhrases.forEach(function(phrase) {
-      if (firstCellText.indexOf(phrase) !== -1) {
+      console.log("Current Phrase:", phrase);
+      if (firstCellText.indexOf(phrase) === 0 || firstCellText.indexOf(' ' + phrase) !== -1) {
         match = true;
       }
     });
