2d15506187869924bead836b4d3c7dfd605512e5
grcodeclub <135416047+GrCodeClub@users.noreply.github.com>
2024-02-17 22:03:23 +0200

    Update search_table_row_cells_0.js

--- search/search_table_row_cells_0.js
+++ search/search_table_row_cells_0.js
@@ -4,18 +4,14 @@ const rows = table.getElementsByTagName("tr");
 
 searchInput.addEventListener("input", function() {
   const searchValue = removeAccents(searchInput.value.toLowerCase());
-  console.log("Search Value:", searchValue);
-  const searchPhrases = searchValue.match(/\b[\w'-]+\b/g); // Φράσεις με βάση τα κενά
-  console.log("Search Phrases:", searchPhrases);
+  const searchWords = searchValue.split(" ");
 
   Array.from(rows).forEach(function(row, index) {
     let match = false;
     const firstCellText = removeAccents(row.cells[0].textContent.toLowerCase());
-    console.log("Cell Text:", firstCellText);
 
-    searchPhrases.forEach(function(phrase) {
-      console.log("Current Phrase:", phrase);
-      if (firstCellText.indexOf(phrase) === 0 || firstCellText.indexOf(' ' + phrase) !== -1) {
+    searchWords.forEach(function(word) {
+      if (firstCellText.includes(word)) {
         match = true;
       }
     });
